cbd383287c9b1a029a6367a0417c48fc3abab14c
Tayfun Ergun <taymcquaya@gmail.com>
2024-08-01 05:11:30 +0800

    reverted Back to the original Default resolution, matched for Free Aspect

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "TEAMMURICA-FLOPYTROMP2D-1.0";
+const contentToCache = [
+    "Build/Unity2DFlap.loader.js",
+    "Build/Unity2DFlap.framework.js",
+    "Build/Unity2DFlap.data",
+    "Build/Unity2DFlap.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
